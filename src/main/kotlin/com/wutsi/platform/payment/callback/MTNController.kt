5d999c6dbf422f86a694bb98388391a34976edf0
htchepannou <htchepannou@expedia.com>
2021-09-13 12:22:49 -0400

    remove get endpoint

--- src/main/kotlin/com/wutsi/platform/payment/callback/MTNController.kt
+++ src/main/kotlin/com/wutsi/platform/payment/callback/MTNController.kt
@@ -1,16 +1,11 @@
 package com.wutsi.platform.payment.callback
 
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 public class MTNController {
-    @GetMapping("/mtn/callback")
-    fun get() {
-    }
-
     @PostMapping("/mtn/callback")
     fun post() {
     }
