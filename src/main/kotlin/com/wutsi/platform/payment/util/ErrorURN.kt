e641e8fe885019bd1f237cb7f1ab11ed070f1d48
wutsi <herve.tchepannou@wutsi.com>
2021-11-19 23:38:44 -0500

    update error urns

--- src/main/kotlin/com/wutsi/platform/payment/util/ErrorURN.kt
+++ src/main/kotlin/com/wutsi/platform/payment/util/ErrorURN.kt
@@ -1,11 +1,7 @@
 package com.wutsi.platform.payment.util
 
-import com.wutsi.platform.core.util.URN
-
 enum class ErrorURN(val urn: String) {
-    GATEWAY_NOT_SUPPORTED(URN.of("error", "payment", "gateway-not-supported").value),
-    TRANSACTION_FAILED(URN.of("error", "payment", "transaction-failed").value),
-    CURRENCY_NOT_SUPPORTED(URN.of("error", "payment", "currency-not-supported").value),
-    ACCOUNT_NOT_FOUND(URN.of("error", "payment", "account-not-found").value),
-    RECIPIENT_CANNOT_RECEIVE_TRANSFER(URN.of("error", "payment", "recipient-cannot-receive-transfer").value),
+    TRANSACTION_FAILED("urn:wutsi:error:payment:transaction-failed"),
+    CURRENCY_NOT_SUPPORTED("urn:wutsi:error:payment:currency-not-supported"),
+    ACCOUNT_NOT_FOUND("urn:wutsi:error:payment:account-not-found"),
 }
