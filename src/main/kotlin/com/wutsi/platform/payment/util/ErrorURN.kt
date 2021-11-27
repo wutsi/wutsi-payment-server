6f414fd8339d2dbb3046a6af2f95703b8fe67a58
wutsi <herve.tchepannou@wutsi.com>
2021-11-27 09:10:28 -0500

    remove un-used errors

--- src/main/kotlin/com/wutsi/platform/payment/util/ErrorURN.kt
+++ src/main/kotlin/com/wutsi/platform/payment/util/ErrorURN.kt
@@ -4,7 +4,6 @@ enum class ErrorURN(val urn: String) {
     TRANSACTION_FAILED("urn:wutsi:error:payment:transaction-failed"),
     TRANSACTION_NOT_FOUND("urn:wutsi:error:payment:transaction-not-found"),
     CURRENCY_NOT_SUPPORTED("urn:wutsi:error:payment:currency-not-supported"),
-    ACCOUNT_NOT_FOUND("urn:wutsi:error:payment:account-not-found"),
     USER_NOT_ACTIVE("urn:wutsi:error:payment:user-not-found"),
     RECIPIENT_NOT_ACTIVE("urn:wutsi:error:payment:recipient-not-found"),
     OWNERSHIP_ERROR("urn:wutsi:error:payment:ownership-error"),
