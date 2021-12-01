1aa58673e29800eeaa6c1ec34d24d4e45d8b237c
wutsi <herve.tchepannou@wutsi.com>
2021-12-01 09:22:16 -0500

    add logs

--- src/main/kotlin/com/wutsi/platform/payment/delegate/SearchTransactionDelegate.kt
+++ src/main/kotlin/com/wutsi/platform/payment/delegate/SearchTransactionDelegate.kt
@@ -1,5 +1,6 @@
 package com.wutsi.platform.payment.`delegate`
 
+import com.wutsi.platform.core.logging.KVLogger
 import com.wutsi.platform.payment.dao.TransactionRepository
 import com.wutsi.platform.payment.dto.SearchTransactionRequest
 import com.wutsi.platform.payment.dto.SearchTransactionResponse
@@ -11,9 +12,14 @@ import org.springframework.stereotype.Service
 @Service
 public class SearchTransactionDelegate(
     private val dao: TransactionRepository,
-    private val tenantProvider: TenantProvider
+    private val tenantProvider: TenantProvider,
+    private val logger: KVLogger,
 ) {
     public fun invoke(request: SearchTransactionRequest): SearchTransactionResponse {
+        logger.add("account_id", request.accountId)
+        logger.add("limit", request.limit)
+        logger.add("offset", request.offset)
+
         val page = PageRequest.of(
             request.offset / request.limit,
             request.limit,
@@ -22,6 +28,7 @@ public class SearchTransactionDelegate(
         val tenantId = tenantProvider.id()
         val txs = dao.findByTenantIdAndAccountId(tenantId, request.accountId, page)
 
+        logger.add("count", txs.size)
         return SearchTransactionResponse(
             transactions = txs.map { it.toTransactionSummary() }
         )
