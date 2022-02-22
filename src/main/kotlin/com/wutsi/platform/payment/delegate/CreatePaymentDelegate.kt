bed6f2fdb322616a05851af9a3d72a75e3f8494a
wutsi <herve.tchepannou@wutsi.com>
2022-02-22 18:36:05 -0500

    remove unused error

--- src/main/kotlin/com/wutsi/platform/payment/delegate/CreatePaymentDelegate.kt
+++ src/main/kotlin/com/wutsi/platform/payment/delegate/CreatePaymentDelegate.kt
@@ -129,13 +129,6 @@ public class CreatePaymentDelegate(
                 )
             )
 
-        if (request.accountId == securityManager.currentUserId())
-            throw ForbiddenException(
-                error = Error(
-                    code = ErrorURN.SELF_TRANSACTION_ERROR.urn
-                )
-            )
-
         val recipient = accounts[request.accountId]!!
         if (!recipient.business)
             throw ForbiddenException(
