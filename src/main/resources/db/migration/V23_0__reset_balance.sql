ffefeac6dfaab1bcbd315f8f5596bfd0c3ddf90e
wutsi <herve.tchepannou@wutsi.com>
2022-06-14 09:04:05 -0400

    fix script

--- src/main/resources/db/migration/V23_0__reset_balance.sql
+++ src/main/resources/db/migration/V23_0__reset_balance.sql
@@ -1,19 +1,20 @@
 DELETE FROM T_BALANCE;
+ALTER TABLE T_BALANCE DROP CONSTRAINT positive_balance;
 
 -- Cash IN
 INSERT INTO T_BALANCE(account_id, tenant_id, amount, currency)
-    SELECT account_id, tenant_id, sum(ABS(net)), currency FROM t_transaction WHERE type=1 AND status=1 GROUP BY account_id, tenant_id, currency;
+    SELECT account_id, tenant_id, sum(net), currency FROM t_transaction WHERE type=1 AND status=1 GROUP BY account_id, tenant_id, currency;
 
 -- Transfer/Charge IN
 UPDATE T_BALANCE SET amount = amount +
     (
-        SELECT COALESCE (sum(ABS(net)), 0)
+        SELECT COALESCE (sum(net), 0)
         FROM T_TRANSACTION
         WHERE (type=3 OR type=4) AND status=1 AND T_BALANCE.account_id=T_TRANSACTION.recipient_id
     );
 
 INSERT INTO T_BALANCE(account_id, tenant_id, amount, currency)
-    SELECT recipient_id, tenant_id, sum(ABS(net)), currency
+    SELECT recipient_id, tenant_id, sum(net), currency
         FROM T_TRANSACTION
         WHERE (type=3 OR type=4) AND status=1 AND recipient_id NOT IN (SELECT account_id FROM T_BALANCE)
         GROUP BY recipient_id, tenant_id, currency;
@@ -22,7 +23,7 @@ INSERT INTO T_BALANCE(account_id, tenant_id, amount, currency)
 -- Cash OUT
 UPDATE T_BALANCE SET amount = amount -
     (
-        SELECT COALESCE (sum(ABS(amount)), 0)
+        SELECT COALESCE (sum(amount), 0)
         FROM T_TRANSACTION
         WHERE type=2 AND status=1 AND T_BALANCE.account_id=T_TRANSACTION.account_id
     );
@@ -30,8 +31,10 @@ UPDATE T_BALANCE SET amount = amount -
 -- Transfer/Charge OUT
 UPDATE T_BALANCE SET amount = amount -
     (
-        SELECT COALESCE (sum(ABS(amount)), 0)
+        SELECT COALESCE (sum(amount), 0)
         FROM T_TRANSACTION
         WHERE (type=3 OR type=4) AND status=1 AND T_BALANCE.account_id=T_TRANSACTION.account_id
     );
 
+UPDATE T_BALANCE SET amount=0 WHERE amount<0;
+ALTER TABLE T_BALANCE ADD CONSTRAINT positive_balance CHECK (amount >= 0);
